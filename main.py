a0283c4fc01288915969d3abdc010add3de07944
Raje Abhilash Mohite (Chavhan Hambirrao) <rajeabhilashmohite@outlook.com>
2023-01-14 12:49:15 +0530

    PDF Extractor - New Table Handler added

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from manipulate.text.pdfs import PDFExctractor
+# from manipulate.visuals import simpleocr
 
 if __name__ == "__main__":
+    # a = simpleocr.get_text_from_image(r"D:\IBA\Projects\BotIQ\sampledata\BasicTable.png",True)
+    # b = simpleocr.get_brackets_to_text(r'D:\IBA\Projects\BotIQ\sampledata\BasicTable.png')
+    # print(a)
+
     arg = {
 
         'file_path': r"D:\Zoetis\UserData\ROW factors - January 2023.pdf",
         'data_table': 'MACHINE SHOP',
-        'is_multi_table': False
+        'is_multi_table': False,
+        'index_page_no' : 1,
     }
 
     obj = PDFExctractor.ExtractPDF(arg)
-    print('No of Pages : ', obj.total_pages())
+
+    # print('No of Pages : ', obj.total_pages())
     # print(obj.read_index(1))
-    print("Page No :", obj.get_page_no_from_index("Abhilash"))
-    obj.extract_data_as_dict()
+    # print("Page No :", obj.get_page_no_by_content("Forge"))
+    obj.extract_single_table_from_page(4)
--- manipulate/text/pdfs/PDFExctractor.py
+++ manipulate/text/pdfs/PDFExctractor.py
@@ -16,7 +16,7 @@ class ExtractPDF(list):
         self.PDF_FILE_PATH = args['file_path']
         self.PDF_DATA_TABLE = args['data_table']
         self.PDF_IS_MULTI_TABLE = args['is_multi_table']
-        # self.PDF_INDEX_PAGE_NO = args['index_page_no']
+        self.PDF_INDEX_PAGE_NO = args['index_page_no']
 
         self.init_pdf_reader()
         self.PDF_INDEX_DATA = self.read_index(self.PDF_INDEX_PAGE_NO)
@@ -35,7 +35,7 @@ class ExtractPDF(list):
     def page_data(self, page):
         return page.extract_text()
 
-    def read_index(self,index_page_number):
+    def read_index(self, index_page_number):
         index = []
         page_data = self.page_data(self.get_page(index_page_number))
         table_of_contents = str(page_data.split('Page')[2])
@@ -45,7 +45,7 @@ class ExtractPDF(list):
                 dt = row.split('.')
                 content = dt[0]
                 page_no = int(dt[len(dt)-1])
-                index.append([content,page_no])
+                index.append([content, page_no])
         return index
 
     def get_page_no_by_content(self, content):
@@ -54,6 +54,20 @@ class ExtractPDF(list):
                 return contents[1]
         return -1
 
-    def extract_data_as_dict(self):
-        pass
+    def extract_single_table_from_page(self, page_no=4):
+        page_data = self.page_data(self.get_page(page_no))
+        table_head_starts_from = '1/80 = 100'
+        table_head_ends_at = 'Date'
+        table_head_start_index = page_data.index(table_head_starts_from)
+        table_head_end_index = page_data.index(table_head_ends_at)
+        print('Length of Page : ', len(page_data))
+        print('Header Start Index', table_head_start_index)
+        print('Header Ends at', table_head_end_index)
+        table_header = page_data[table_head_start_index+len(table_head_starts_from):table_head_end_index]
+        table_header = table_header.replace(' ','~')
+        print('Table Header ', table_header)
 
+
+        # lines = page_data.splitlines()
+        # for line in lines:
+        #     print(line)
